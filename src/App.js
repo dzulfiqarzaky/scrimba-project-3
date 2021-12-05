1005e8353cc4c317efa496fee46909570914cb63
zaky <dzulfiqarakyaulia@gmail.com>
2021-12-06 06:00:56 +0700

    update card based on data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/card"
+import data from "./data"
+
 /*
 Challenge: Build the Hero component.
 Check the Figma file for the design specifics.
 */
 
 export default function App() {
+    let dataCard = data.map(data => {
+        return <Card 
+        img = {data.coverImg}
+        rating = {data.stats.rating}
+        review = {data.stats.reviewCount}
+        title = {data.title}
+        price = {data.price}
+        location = {data.location}
+        />
+    })
     return (
         <div>
             <Navbar />
             <Hero />
+            {dataCard}
         </div>
     )
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,21 @@
+import React from "react"
 
-
-export default Card () {
+export default function Card (props) {
     return (
-        
+        <>
+        <div className="card flex">
+            <img className="img-card" src={`../images/${props.img}`}/>
+            <div className="flex rating">
+                <div class="img-star"><img className="" src="../images/star.png" alt=""/></div>
+                <p>{props.rating}</p>
+                <p>({props.reviewCount}})</p>
+                <p>{props.location}</p>
+            </div>
+            <div class="text">
+                <p>{props.title}</p>
+                <p><strong>From $${props.price}</strong> / person</p>
+            </div>
+        </div>
+        </>
     )
 }
